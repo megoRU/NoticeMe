7f19219171dee1d2d96f6d624d0ad9ff404b07e4
megoru <savin.yura2014@gmail.com>
2022-11-22 14:33:35 +0300

    Fix #4

--- src/main/java/main/event/UserJoinEvent.java
+++ src/main/java/main/event/UserJoinEvent.java
@@ -10,6 +10,7 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -29,26 +30,36 @@ public class UserJoinEvent extends ListenerAdapter {
 
         Guild guild = event.getGuild();
         User user = event.getMember().getUser();
-        VoiceChannel voiceChannel = event.getChannelJoined().asVoiceChannel();
-        NoticeRegistry instance = NoticeRegistry.getInstance();
 
-        TrackingUser instanceUser = instance.getUser(guild.getId(), user.getId());
+        try {
+            VoiceChannel voiceChannel = getAsChannel(event.getChannelJoined());
+            NoticeRegistry instance = NoticeRegistry.getInstance();
 
-        if (instanceUser == null) return;
-        String userList = instanceUser.getUserList();
-        Optional<Server> guildOptional = guildRepository.findById(guild.getIdLong());
+            TrackingUser instanceUser = instance.getUser(guild.getId(), user.getId());
 
-        if (guildOptional.isPresent()) {
-            Server guildDB = guildOptional.get();
-            TextChannel textChannel = event.getGuild().getTextChannelById(guildDB.getTextChannelId());
-            if (textChannel != null) {
-                String text = String.format(jsonParsers.getTranslation("user_enter_to_channel", guild.getId()),
-                        user.getId(),
-                        voiceChannel.getId(),
-                        userList);
+            if (instanceUser == null) return;
+            String userList = instanceUser.getUserList();
+            Optional<Server> guildOptional = guildRepository.findById(guild.getIdLong());
 
-                textChannel.sendMessage(text).queue();
+            if (guildOptional.isPresent()) {
+                Server guildDB = guildOptional.get();
+                TextChannel textChannel = event.getGuild().getTextChannelById(guildDB.getTextChannelId());
+                if (textChannel != null) {
+                    String text = String.format(jsonParsers.getTranslation("user_enter_to_channel", guild.getId()),
+                            user.getId(),
+                            voiceChannel.getId(),
+                            userList);
+
+                    textChannel.sendMessage(text).queue();
+                }
             }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+    }
+
+    private VoiceChannel getAsChannel(AudioChannelUnion audioChannelUnion) {
+        if (audioChannelUnion instanceof VoiceChannel) return audioChannelUnion.asVoiceChannel();
+        else throw new IllegalArgumentException(audioChannelUnion.getName() + " is not a VoiceChannel!");
     }
 }
