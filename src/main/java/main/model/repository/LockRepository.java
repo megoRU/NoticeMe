fddb61c36270bc2dbfa44aab43bd06a62705f0b2
megoRU <savin.yura2014@gmail.com>
2023-06-09 19:55:05 +0300

    1. Some fix

--- src/main/java/main/model/repository/LockRepository.java
+++ src/main/java/main/model/repository/LockRepository.java
@@ -2,12 +2,14 @@ package main.model.repository;
 
 import main.model.entity.Lock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface LockRepository extends JpaRepository<Lock, Long> {
 
+    @Modifying
     @Transactional
     void deleteLockByUserId(long userId);
 }
