30169168fdcccd06c72f9576dc2729537cc4567b
megoRU <savin.yura2014@gmail.com>
2025-04-30 16:25:25 +0300

    Fix

--- src/main/java/main/core/events/SuggestionCommand.java
+++ src/main/java/main/core/events/SuggestionCommand.java
@@ -33,49 +33,41 @@ public class SuggestionCommand {
         var user = event.getUser();
 
         Set<String> suggestions = instance.getSuggestionsList(guildIdString, user.getId());
+        List<String> top5Users = suggestions.stream().limit(5).toList();
 
-        List<String> top5Users = suggestions.stream()
-                .limit(5)
-                .toList();
+        StringBuilder stringBuilder = new StringBuilder();
+        List<Button> buttonsList = new ArrayList<>();
 
-        if (!top5Users.isEmpty()) {
-            StringBuilder stringBuilder = new StringBuilder();
-            List<Button> buttonsList = new ArrayList<>();
+        for (int i = 0; i < top5Users.size(); i++) {
+            if (stringBuilder.isEmpty()) {
+                stringBuilder.append((i + 1)).append(". ").append("<@").append(top5Users.get(i)).append(">");
+            } else {
+                stringBuilder.append("\n").append((i + 1)).append(". ").append("<@").append(top5Users.get(i)).append(">");
+            }
 
-            for (int i = 0; i < top5Users.size(); i++) {
-                if (stringBuilder.isEmpty()) {
-                    stringBuilder.append((i + 1)).append(". ").append("<@").append(top5Users.get(i)).append(">");
-                } else {
-                    stringBuilder.append("\n").append((i + 1)).append(". ").append("<@").append(top5Users.get(i)).append(">");
-                }
-
-                Member member = guild.getMemberById(top5Users.get(i));
-                if (member == null) {
-                    try {
-                        member = guild.retrieveMemberById(top5Users.get(i)).complete();
-                        String addUser = String.format(jsonParsers.getTranslation("add_user", guildIdString), member.getEffectiveName());
-                        buttonsList.add(Button.primary(NoticeMeUtils.BUTTON_ADD_USER + top5Users.get(i), addUser));
-                    } catch (ErrorResponseException e) {
-                        if (e.getErrorCode() == 10007) { // UNKNOWN_MEMBER
-                            LOGGER.info("UNKNOWN_MEMBER: {} Guild: {}", top5Users.get(i), guildIdString);
-                        }
-                    }
-                } else {
+            Member member = guild.getMemberById(top5Users.get(i));
+            if (member == null) {
+                try {
+                    member = guild.retrieveMemberById(top5Users.get(i)).complete();
                     String addUser = String.format(jsonParsers.getTranslation("add_user", guildIdString), member.getEffectiveName());
                     buttonsList.add(Button.primary(NoticeMeUtils.BUTTON_ADD_USER + top5Users.get(i), addUser));
+                } catch (ErrorResponseException e) {
+                    if (e.getErrorCode() == 10007) { // UNKNOWN_MEMBER
+                        LOGGER.info("UNKNOWN_MEMBER: {} Guild: {}", top5Users.get(i), guildIdString);
+                    }
                 }
-            }
-
-            if (buttonsList.isEmpty()) {
-                String noSuggestions = jsonParsers.getTranslation("no_suggestions", guildIdString);
-                event.reply(noSuggestions).setEphemeral(true).queue();
             } else {
-                String suggestionText = String.format(jsonParsers.getTranslation("suggestion_text", guildIdString), stringBuilder);
-                event.reply(suggestionText).setActionRow(buttonsList).setEphemeral(true).queue();
+                String addUser = String.format(jsonParsers.getTranslation("add_user", guildIdString), member.getEffectiveName());
+                buttonsList.add(Button.primary(NoticeMeUtils.BUTTON_ADD_USER + top5Users.get(i), addUser));
             }
-        } else {
+        }
+
+        if (buttonsList.isEmpty()) {
             String noSuggestions = jsonParsers.getTranslation("no_suggestions", guildIdString);
             event.reply(noSuggestions).setEphemeral(true).queue();
+        } else {
+            String suggestionText = String.format(jsonParsers.getTranslation("suggestion_text", guildIdString), stringBuilder);
+            event.reply(suggestionText).setActionRow(buttonsList).setEphemeral(true).queue();
         }
     }
 }
